3523ba42f45a3a8952fcaaf30142aeff79268895
profeRonaldSena <ronald.barrera@cavirey.edu.co>
2023-04-27 08:40:28 -0500

    modified:   src/components/Formulario.js

--- src/components/Formulario.js
+++ src/components/Formulario.js
@@ -1,18 +1,38 @@
+import styled from '@emotion/styled'
+
+const DIV = styled.div`
+ display: flex;
+ margin-bottom: 1rem;
+ align-items: center;
+`
+const LABEL = styled.label`
+ flex: 0 0 100px;
+`
+const SELECT = styled.select`
+ display: block;
+ width: 100%;
+ padding: 1rem;
+ border: 1px solid #e1e1e1;
+ -webkit-appearance: none;
+`
+const INPUT = styled.input`
+ margin: 0 1rem;
+`
 const Formulario = () => {
  return (
   <form>
-   <div>
-    <label>Marca</label>
-    <select>
+   <DIV>
+    <LABEL>Marca</LABEL>
+    <SELECT>
      <option value=''>---Seleccione---</option>
      <option value='americano'>Americano</option>
      <option value='europeo'>Europeo</option>
      <option value='asiatico'>Asiatico</option>
-    </select>
-   </div>
-   <div>
-    <label>Año</label>
-    <select>
+    </SELECT>
+   </DIV>
+   <DIV>
+    <LABEL>Año</LABEL>
+    <SELECT>
      <option value='2023'>2023</option>
      <option value='2022'>2022</option>
      <option value='2021'>2021</option>
@@ -22,15 +42,16 @@ const Formulario = () => {
      <option value='2017'>2017</option>
      <option value='2016'>2016</option>
      <option value='2015'>2015</option>
-    </select>
-   </div>
-   <div>
-    <label>Plan</label>
-    <input type='radio' name='plan' value='basico' />
+    </SELECT>
+   </DIV>
+   <DIV>
+    <LABEL>Plan</LABEL>
+    <INPUT type='radio' name='plan' value='basico' />
     Basico
-    <input type='radio' name='plan' value='completo' />
+    <INPUT type='radio' name='plan' value='completo' />
     Completo
-   </div>
+   </DIV>
+   <button type='button'>Cotizar</button>
   </form>
  )
 }
